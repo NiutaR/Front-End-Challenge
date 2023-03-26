695b4ff7035315b553149516d89a26c0bfe0ba66
NiutaR <69516148+NiutaR@users.noreply.github.com>
2023-03-26 13:45:31 +0300

    feat: Change the color of the pressed key

--- src/components/Keybord/Row/Key/Key.jsx
+++ src/components/Keybord/Row/Key/Key.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import keyStyles from './Key.module.css';
 
 const Key = ({ label, value, className }) => {
+  const [keyState, setKeyState] = useState('default');
+
+  const handleMouseDown = () => {
+    setKeyState('pressed');
+  };
+
+  const handleMouseUp = () => {
+    setKeyState('released');
+  };
+
+  const handleMouseLeave = () => {
+    setKeyState('default');
+  };
+
   const keyClassNames = classNames(keyStyles.key, {
     [keyStyles['key--double']]: Array.isArray(value),
     [keyStyles['key--caps']]: label === 'CapsLock',
@@ -12,11 +26,16 @@ const Key = ({ label, value, className }) => {
     [keyStyles['key--shift']]: label === 'Shift',
     [keyStyles['key--enter']]: label === 'Enter',
     [keyStyles['key--lock']]: label === 'NumLock',
+    [keyStyles[`key--${keyState}`]]: true,
     [className]: className
   });
 
   return (
-    <button className={keyClassNames}>
+    <button className={keyClassNames}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseLeave}
+      tabIndex={0}>
       {Array.isArray(value) ? value.join('\n') : value}
       <div className={keyStyles.label}>{label}</div>
     </button>
